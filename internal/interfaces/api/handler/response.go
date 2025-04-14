a5c1ff7095c599204d52c79370fa04007128d1ac
Hiro <80455873+zzhunght@users.noreply.github.com>
2025-04-14 23:27:19 +0700

    day 1

---
+++ internal/interfaces/api/handler/response.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func successResponse(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
+
+func errorResponse(w http.ResponseWriter, err error, status int) {
+	data := &Response{
+		Message: err.Error(),
+		Status:  status,
+	}
+	js, err := json.Marshal(&data)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+}
